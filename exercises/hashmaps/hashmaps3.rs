b30f3147e607db0ff814ec082cbb2b2add6af47d
finntew <nanomoa76@gmail.com>
2024-09-21 21:15:41 +0800

    update

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
@@ -34,18 +32,26 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
-        // TODO: Populate the scores table with details extracted from the
-        // current line. Keep in mind that goals scored by team_1
-        // will be the number of goals conceded from team_2, and similarly
-        // goals scored by team_2 will be the number of goals conceded by
-        // team_1.
+
+        let mut t1_scored = team_1_score;
+        let mut t2_scored = team_2_score;
+
+        if scores.contains_key(&team_1_name) {
+            t1_scored += scores.get(&team_1_name).unwrap().goals_scored;
+            t2_scored += scores.get(&team_1_name).unwrap().goals_conceded;
+        }
+        if scores.contains_key(&team_2_name) {
+            t1_scored += scores.get(&team_2_name).unwrap().goals_conceded;
+            t2_scored += scores.get(&team_2_name).unwrap().goals_scored;
+        }
+
         scores.insert(team_1_name, Team {
-            goals_scored: team_1_score,
-            goals_conceded: team_2_score
+            goals_scored: t1_scored,
+            goals_conceded: t2_scored
         });
         scores.insert(team_2_name, Team {
-            goals_scored: team_2_score,
-            goals_conceded: team_1_score
+            goals_scored: t2_scored,
+            goals_conceded: t1_scored
         });
     }
     scores
